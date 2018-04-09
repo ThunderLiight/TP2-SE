e76f0cf9956d918da12da86bb4f4c8e3c3d4ea29
ThunderLiight <37722356+ThunderLiight@users.noreply.github.com>
2018-04-08 21:36:39 -0400

    Update TP2-3-A.sh

--- TP2-3-A.sh
+++ TP2-3-A.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+echo "Saisissez un répertoire source: "
+read x
+
+echo "Saisissez un répertoire cible: "
+read y
+
+echo "Saisissez une taille d'image: "
+read z
+
+for image in ($x.jpeg $x.gif $x.png)
+do convert -thumbnail z abc.png thumb.abc.png > y
+done
